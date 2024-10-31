12d3851ba76e4d24f08a756d53236ee8893c3997
dongkyu <skehd133@naver.com>
2024-10-31 13:20:08 +0900

    오타수정

--- libreDwgOsx.cpp
+++ libreDwgOsx.cpp
@@ -912,7 +912,7 @@ std::string ExtractDWGData(Dwg_Data* dwg, Napi::Array jsonArr, Napi::Env env) {
   Napi::Array viewPosition = checkObject(jsonObj,"viewPosition", env);
   
   viewPosition.Set(zero, Napi::Number::New(env, modelxmax - (modelxmax-modelxmin)/2));
-  viewPosition.Set(1, Napi::Number::New(env, (modelymax - modelymax-modelymin)/2));
+  viewPosition.Set(1, Napi::Number::New(env, modelymax - (modelymax-modelymin)/2));
   viewPosition.Set(2, Napi::Number::New(env, (modelzmax-modelzmin)/2));
   // jsonObj.Set("viewPosition", viewPosition);
   return std::string(dwg_version_type(dwg->header.from_version));
